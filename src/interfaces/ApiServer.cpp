77aec2ede6a347e0df59febbcd44114f831accf8
serobcholakyan116-crypto <serobcholakyan116@gmail.com>
2026-04-12 10:48:35 -0700

    Create ApiServer.cpp

---
+++ src/interfaces/ApiServer.cpp
@@ -0,0 +1,10 @@
+#include "ApiServer.hpp"
+#include <iostream>
+
+void ApiServer::start() {
+    std::cout << "API Server started." << std::endl;
+}
+
+void ApiServer::stop() {
+    std::cout << "API Server stopped." << std::endl;
+}
