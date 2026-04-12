9e42d0f3f790639d96dc701630382848b6584e72
serobcholakyan116-crypto <serobcholakyan116@gmail.com>
2026-04-12 11:53:42 -0700

    Update AuthenticationService.hpp

--- src/services/AuthenticationService.hpp
+++ src/services/AuthenticationService.hpp
@@ -1,14 +1,16 @@
 #pragma once
 #include <string>
-#include "SystemConfig.hpp"
+#include "../common/SystemConfig.hpp"
+#include "../sim/SimManager.hpp"
 
 class AuthenticationService {
 public:
-    AuthenticationService(const SystemConfig& config);
+    AuthenticationService(const SystemConfig& config, SimManager& simManager);
 
-    bool authenticateDevice(const std::string& deviceId);
+    bool authenticateDevice(const std::string& deviceId, const std::string& simId);
     void revokeAccess(const std::string& deviceId);
 
 private:
     SystemConfig config;
+    SimManager& simManager;
 };
