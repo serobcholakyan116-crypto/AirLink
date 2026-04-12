4ff786024c62e564ceab911c0869f55bdfc92ed6
serobcholakyan116-crypto <serobcholakyan116@gmail.com>
2026-04-12 10:29:31 -0700

    Create connection-manager.py

---
+++ src/core/connection-manager.py
@@ -0,0 +1,15 @@
+class ConnectionManager:
+    def __init__(self, config):
+        self.config = config
+
+    def start_session(self, device_id, user_context):
+        # TODO: implement session creation logic
+        pass
+
+    def end_session(self, session_id):
+        # TODO: implement session termination logic
+        pass
+
+    def get_active_sessions(self):
+        # TODO: return active sessions
+        return []
