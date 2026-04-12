5c0574173d62c642235016eb92c52b53863682a5
serobcholakyan116-crypto <serobcholakyan116@gmail.com>
2026-04-12 10:49:56 -0700

    Create AdminPanel.cpp

---
+++ src/interfaces/AdminPanel.cpp
@@ -0,0 +1,10 @@
+#include "AdminPanel.hpp"
+#include <iostream>
+
+void AdminPanel::loadDashboard() {
+    std::cout << "Admin dashboard loaded." << std::endl;
+}
+
+void AdminPanel::showSystemStatus() {
+    std::cout << "Displaying system status..." << std::endl;
+}
