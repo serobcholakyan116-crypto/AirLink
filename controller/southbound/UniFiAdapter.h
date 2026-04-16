d8af9286be58a592d6f7fc9162971f3a4b227abd
serobcholakyan116-crypto <serobcholakyan116@gmail.com>
2026-04-16 16:34:43 -0700

    Update UniFiAdapter.h

--- controller/southbound/UniFiAdapter.h
+++ controller/southbound/UniFiAdapter.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "DeviceAdapter.h"
+#include <string>
 
 class UniFiAdapter : public DeviceAdapter {
+private:
+    std::string base_url;
+    std::string api_key;
+
 public:
+    UniFiAdapter(const std::string& url, const std::string& key);
+
     void applyConfig(const APCommand& cmd) override;
 };
