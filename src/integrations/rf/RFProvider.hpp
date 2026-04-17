cb32a1ca5df17b8d21a0ab5f164cb7cedb219704
serobcholakyan116-crypto <serobcholakyan116@gmail.com>
2026-04-16 18:24:00 -0700

    Update RFProvider.hpp

--- src/integrations/rf/RFProvider.hpp
+++ src/integrations/rf/RFProvider.hpp
@@ -8,5 +8,23 @@ public:
     virtual bool authenticate() = 0;
     virtual void setChannel(const std::string& apId, int channel) = 0;
     virtual void setPower(const std::string& apId, int dbm) = 0;
-    virtual std::string getClients() = 0;
+    virtual std::string getClients() = class UniFiRF : public RFProvider {
+    UniFiClient client;
+
+public:
+    UniFiRF(UniFiClient c) : client(c) {}
+
+    bool authenticate() override {
+        return client.login();
+    }
+
+    void setChannel(const std::string& apId, int channel) override {
+        // API call to UniFi
+    }
+
+    void setPower(const std::string& apId, int dbm) override {}
+
+    std::string getClients() override {
+        return client.getClients();
+    }
 };
