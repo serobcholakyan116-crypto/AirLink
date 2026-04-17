f5cddfcd71ed8a57c7bd6e5bac5466ffd783c222
serobcholakyan116-crypto <serobcholakyan116@gmail.com>
2026-04-16 17:51:07 -0700

    Update RfControllar.hpp

--- src/rf/RfControllar.hpp
+++ src/rf/RfControllar.hpp
@@ -4,23 +4,33 @@
 
 struct AccessPoint {
     std::string id;
-    std::string ip;
+    std::string mac;
     int channel;
     int txPower;
-    bool online;
 };
 
 class RfController {
 public:
+    RfController(const std::string& baseUrl,
+                 const std::string& username,
+                 const std::string& password,
+                 const std::string& site);
+
+    bool login();
+
     void registerAP(const AccessPoint& ap);
 
-    void pushSSID(const std::string& ssid);
-    void setChannel(const std::string& apId, int channel);
-    void setTxPower(const std::string& apId, int power);
-
-    void applyConfig();
-    std::vector<AccessPoint> getAPs() const;
+    bool createOpenSSID(const std::string& ssid);
+    bool setChannel(const std::string& mac, int channel);
+    bool setTxPower(const std::string& mac, int power);
 
 private:
+    std::string baseUrl;
+    std::string username;
+    std::string password;
+    std::string site;
+
     std::vector<AccessPoint> aps;
+
+    std::string httpPost(const std::string& url, const std::string& payload);
 };
